35433036b5ed98d4d15c9522d547192cfbe2320b
sjuknelis <81927579+sjuknelis@users.noreply.github.com>
2022-02-01 17:05:41 -0500

    Create BasicAutoSystem.java

---
+++ BasicAutoSystem.java
@@ -0,0 +1,18 @@
+// Basically how the auto is gonna work (not finished)
+
+strafeRight(1.0);
+moveForward(0.8);
+// TODO: dump
+turnRight();
+strafeRight(0.5);
+moveBackward(2);
+// TODO: spin corner
+moveBackward(1.0);
+turnRight();
+turnRight();
+strafeRight(0.2);
+moveBackward(3.5);
+moveForward(1.0);
+turnRight();
+moveForward(1.0);
+strafeRight(1.0);
